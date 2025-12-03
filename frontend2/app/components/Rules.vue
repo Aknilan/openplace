c294f94e444a747437673b56ae237400d130a669
Adam Demasi <adam@hbang.com.au>
2025-12-03 20:27:51 +1030

    Nicer layout for rules list

--- frontend2/app/components/Rules.vue
+++ frontend2/app/components/Rules.vue
@@ -3,30 +3,30 @@
 		Loading…
 	</p>
 
-	<ul v-else>
-		<li v-if="rules.isMultiAccountAllowed">You may create more than one account.</li>
-		<li v-else>Do not create more than one account. If multiple accounts are detected, all accounts will be banned.</li>
+	<ul v-else class="rules-list">
+		<li v-if="rules.isMultiAccountAllowed"><i>🔑</i> You may create more than one account.</li>
+		<li v-else><i>🔑</i> Do not create more than one account. If multiple accounts are detected, all accounts will be banned.</li>
 
-		<li v-if="rules.isOffensiveContentAllowed">You may draw content that is likely to offend others.</li>
-		<li v-else>Do not draw offensive content.</li>
+		<li v-if="rules.isOffensiveContentAllowed"><i>🫢</i> You may draw content that is likely to offend others.</li>
+		<li v-else><i>🫢</i> Do not draw offensive content.</li>
 
-		<li v-if="rules.isExplicitContentAllowed">You may draw explicit or suggestive content.</li>
-		<li v-else>Do not draw content that is explicit or suggestive.</li>
+		<li v-if="rules.isExplicitContentAllowed"><i>🔞</i> You may draw explicit or suggestive content.</li>
+		<li v-else><i>🔞</i> Do not draw content that is explicit or suggestive.</li>
 
-		<li v-if="rules.isGriefingAllowed">You may draw over existing artwork to obscure it (griefing).</li>
-		<li v-else>Do not draw over existing artwork to obscure it (griefing).</li>
+		<li v-if="rules.isGriefingAllowed"><i>🖍️</i> You may draw over existing artwork to obscure it (griefing).</li>
+		<li v-else><i>🖍️</i> Do not draw over existing artwork to obscure it (griefing).</li>
 
-		<li v-if="rules.isKindGriefingAllowed">You may draw over existing artwork if you are complementing it.</li>
-		<li v-else>Do not modify existing artwork, even to improve it.</li>
+		<li v-if="rules.isKindGriefingAllowed"><i>🤝</i> You may draw over existing artwork if you are complementing it.</li>
+		<li v-else><i>🤝</i> Do not modify existing artwork, even to improve it.</li>
 
-		<li v-if="rules.isPoliticalGriefingAllowed">You may draw over political flags or portraits.</li>
+		<li v-if="rules.isPoliticalGriefingAllowed"><i>🏴‍☠️</i> You may draw over political flags or portraits.</li>
 
-		<li v-if="!rules.isVPNAllowed">Use of VPNs or proxies is not allowed on this instance.</li>
+		<li v-if="!rules.isVPNAllowed"><i>🛜</i> Use of VPNs or proxies is not allowed on this instance.</li>
 
-		<li v-if="rules.isBottingAllowed">You may use bots to automate drawing on this instance.</li>
-		<li v-else>Use of bots or any other automation is not allowed on this instance.</li>
+		<li v-if="rules.isBottingAllowed"><i>🤖</i> You may use bots to automate drawing on this instance.</li>
+		<li v-else><i>🤖</i> Use of bots or any other automation is not allowed on this instance.</li>
 
-		<li>Do not create excessive traffic that may affect other people’s experience on this instance.</li>
+		<li><i>⚠️</i> Do not create excessive traffic that may affect other people’s experience on this instance.</li>
 	</ul>
 
 	<p v-if="rules?.extraRules">{{ rules?.extraRules }}</p>
@@ -70,5 +70,22 @@ const loadRules = async () => {
 </script>
 
 <style scoped>
-/* */
+.rules-list {
+	display: flex;
+	flex-direction: column;
+	gap: 0.5rem;
+	list-style: none;
+	padding: 0;
+}
+
+.rules-list li {
+	display: flex;
+	flex-direction: row;
+	align-items: first baseline;
+}
+
+.rules-list i {
+	font-style: normal;
+	margin-right: 0.75rem;
+}
 </style>
